916b538fe8fab40a3e55094b839a80a6019fbf3b
Chunxu Tang <chunxutang@gmail.com>
2023-09-07 14:55:24 -0700

    Add worker ring pytests

--- alluxio-python-library/setup.py
+++ alluxio-python-library/setup.py
@@ -1,14 +1,19 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
 
 setup(
-    name='alluxio-python-library',
-    version='0.1',
-    packages=find_packages(),
+    name="alluxio-python-library",
+    version="0.1",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    include_package_data=True,
+    zip_safe=False,
     install_requires=[
-        'humanfriendly',
-        'requests',
-        'etcd3',
-        'mmh3',
-        'sortedcontainers',
-    ]
+        "humanfriendly",
+        "requests",
+        "etcd3",
+        "mmh3",
+        "sortedcontainers",
+    ],
+    extras_require={"tests": ["pytest"]},
+    python_requires=">=3.8",
 )
